134bd40973be8b189e4c292ba329b5af7dfea5b6
sajal <mdsajalcse@gmail.com>
2023-08-25 21:03:06 +0600

    update mul table

--- multiplicationTable.c
+++ multiplicationTable.c
@@ -4,14 +4,13 @@ int main(){
    int i,num;
    printf("Enter any Number ::");
    scanf("%d",&num);
-   for(;;)
-    {
+
    for(i=1;i<=10;i++){
 
     printf("%2d X %2d = %2d\n",i,num,num*i);
-    break;
+
    }
-   }
+
 
 return 0;
 }
