a87ee567b21bda417f5d0a2659361531b8db7ec4
sajal <mdsajalcse@gmail.com>
2023-08-25 21:47:54 +0600

    Loop Variations 1

---
+++ forLoopVariations1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(){
+
+
+   int i,j;
+
+   for(printf("Enter i value::"),scanf("%d",&i),printf("Enter j Value::"),scanf("%d",&j);i<=10;i++){
+
+    printf("i=%d , j=%d\n",i,j);
+    j=j+2;
+
+
+
+
+  }
+
+
+return 0;
+}
